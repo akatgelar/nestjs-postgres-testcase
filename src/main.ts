d3aa1f7378cc95805f27efb812d103df46f7d202
akatgelar <akatgelar@gmail.com>
2025-02-02 15:23:16 +0700

    feat: swagger & response intercept

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ResponseInterceptor } from './utils/response.interceptor';
+
+const configService = new ConfigService();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // response
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
+  // swagger
+  const options = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('Your API description')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .addServer('https://staging.yourapi.com/', 'Staging')
+    .addServer('https://production.yourapi.com/', 'Production')
+    .addTag('Your API Tag')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('', app, document);
+  await app.listen(configService.get<number>('APP_PORT') ?? 3000);
 }
 bootstrap();
---
+++ src/utils/response.interceptor.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  HttpException,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    // const request = context.switchToHttp().getRequest();
+    const response = context.switchToHttp().getResponse();
+    const statusCode = response.statusCode;
+
+    return next.handle().pipe(
+      map((data) => ({
+        statusCode,
+        message: statusCode >= 400 ? 'Error' : 'Success',
+        error: statusCode >= 400 ? response.message : null,
+        timestamp: Date.now(),
+        // version: 'v2',
+        // path: request.url,
+        data,
+      })),
+      catchError((err) => {
+        const statusCode = err instanceof HttpException ? err.getStatus() : 500;
+        const errorResponse = {
+          statusCode,
+          message: err.message || 'Internal server error',
+          error: err.name || 'Error',
+          timestamp: Date.now(),
+          // version: 'v2',
+          // path: request.url,
+          data: {},
+        };
+        return throwError(() => new HttpException(errorResponse, statusCode));
+      }),
+    );
+  }
+}
